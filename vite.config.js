ecd27983b37dfe2dcf4cee17e39e2feb1576a608
Tanner <tannerhlyon@gmail.com>
2023-03-06 19:24:23 -0800

    a/b testing issues

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import path from 'path';
 // import { chromeExtension } from 'vite-plugin-chrome-extension';
 
 export default defineConfig({
-  // plugins: [solidPlugin({ dev: true })],
+  // plugins: [html()],
   base: '/Tanner-Lyon-Portfolio/',
-  // build: {
-  //   lib: {
-  //     entry: path.resolve(__dirname, './Client/index.html'),
-  //     name: 'MyLib',
-  //     fileName: (format) => `my-lib.${format}.js`
-  //   },
+  build: {
+    lib: {
+      // entry: path.resolve(__dirname, './Client/index.html'),
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
+    },
     extensions: [
       ".js",
       ".jsx",
